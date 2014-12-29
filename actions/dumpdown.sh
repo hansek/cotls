7df6ae51d37b88e7e5161ee5517282e8ea567f11
Jan Tezner <jan.tezner@gmail.com>
2014-12-29 17:36:21 +0100

    Add basic validation for "dumpdown" action

--- actions/dumpdown.sh
+++ actions/dumpdown.sh
@@ -22,9 +22,19 @@ dumpdown() {
             loge "MODX Revolution config.inc.php not found for remote CORE path \"${REMOTE_MODX_CORE}\""
         fi
 
+        log "Loading remote database credentials from MODX Revolution config file"
+
         DB_REMOTE_USER=`ssh ${SSH_USER}@${SSH_SERVER} cat ${REMOTE_MODX_CORE}${MODX_CONFIG_PATH} | grep \\$database_user | cut -d \' -f 2`
         DB_REMOTE_PASS=`ssh ${SSH_USER}@${SSH_SERVER} cat ${REMOTE_MODX_CORE}${MODX_CONFIG_PATH} | grep \\$database_password | cut -d \' -f 2`
         DB_REMOTE_NAME=`ssh ${SSH_USER}@${SSH_SERVER} cat ${REMOTE_MODX_CORE}${MODX_CONFIG_PATH} | grep \\$dbase | cut -d \' -f 2`
+
+        logs "Done, loading of credentials complete"
+    fi
+
+    # main variables validation
+    if [ -z "${DB_REMOTE_USER}" ] || [ -z "${DB_REMOTE_PASS}" ] || [ -z "${DB_REMOTE_NAME}" ]
+    then
+        loge "At least one of username, password or database name of remote database is not set"
     fi
 
     TARGET_FILENAME=${DB_REMOTE_NAME}${CONFIG_SUFFIX}.$(date +"%Y-%m-%d-%H%M").sql.gz
