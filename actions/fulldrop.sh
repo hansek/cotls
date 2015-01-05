660557988cfcd6c12926661fb684e2799e1407f3
Jan Tezner <jan.tezner@gmail.com>
2015-01-05 13:58:41 +0100

    Fix missing -p parameter for mysql on fulldrop action

--- actions/fulldrop.sh
+++ actions/fulldrop.sh
@@ -8,7 +8,7 @@ fulldrop() {
 
     if [ ! -z ${DB_LOCAL_PASS+x} ]
     then
-        PASSWORD="${DB_LOCAL_PASS}"
+        PASSWORD="-p${DB_LOCAL_PASS}"
     fi
 
     log "Droping all tables from database \"${DB_LOCAL_NAME}\""
