063bb6432d76ae60c57ecf05fb72df77e70b9623
Jan Tezner <jan.tezner@gmail.com>
2015-01-05 13:45:34 +0100

    Fix error when using password on local database

--- actions/import.sh
+++ actions/import.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ACTION_NAME="import"
-ACTION_VERSION="2014-12-24"
+ACTION_VERSION="2015-01-05"
 
 import() {
     PASWORD=""
 
     if [ ! -z ${DB_LOCAL_PASS+x} ]
     then
-        PASSWORD="${PASSWORD}"
+        PASSWORD="-p${PASSWORD}"
     fi
 
     log "Importing file \e[37;44m${FILE_TO_IMPORT}\e[0m into database \e[37;44m${DB_LOCAL_NAME}\e[0m"
@@ -16,7 +16,7 @@ import() {
     # zip archive
     if [[ ${FILE_TO_IMPORT} == *.zip ]]
     then
-        unzip -p ${FILE_TO_IMPORT} | mysql -u ${DB_LOCAL_USER} ${DB_LOCAL_NAME}
+        unzip -p ${FILE_TO_IMPORT} | mysql -u ${DB_LOCAL_USER} ${PASSWORD} ${DB_LOCAL_NAME}
 
     # gzip archive
     elif [[ ${FILE_TO_IMPORT} == *.gz ]]; then
